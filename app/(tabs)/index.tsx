1a7079a838df665195477f1e8ad630e7a2d18eb0
unknown <mboti20000@gmail.com>
2026-03-28 17:39:54 +0100

    Add new assets, update screens, and add new components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreView);
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -26,6 +30,6 @@ export default function Index() {
         Got to Sign up
       </Link>
       <Link href="/(tabs)/subscriptions/spotify">Spotify subscription</Link>
-    </View>
+    </SafeAreaView>
   );
 }
